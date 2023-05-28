c482e85e7306ef451dd892351a01fc6ce778ff26
John Gorriceta <79299314+jongee0w0@users.noreply.github.com>
2023-05-27 23:53:04 -0600

    changed div size

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -5,7 +5,7 @@ export default function Results(){
     return(
     <main className="flex h-full flex-col items-center justify-between">
       <div className='flex h-full w-full flex-col items-center'>
-        <div className='flex flex-col w-full h-3/4 bg-secondary p-6'>
+        <div className='flex flex-col w-full h-1/2 bg-secondary p-6'>
           <h1 className="text-white text-2xl font-black">Results</h1>
         </div>
         <div className="h-1/3 w-full bg-wave bg-no-repeat">
